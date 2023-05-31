526504c55cab2dc9b38e63ed24b6fe07bd2bbfc2
paosanchezmonroy <paola_sanchez_monroy@hotmail.com>
2023-05-31 15:52:53 -0600

    rutas botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
+import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
-  {path: 'generos',component: IndiceGenerosComponent}
+  {path: 'generos',component: IndiceGenerosComponent},
+  {path: 'generos/crear', component: CrearGeneroComponent},
 
 ];
 
