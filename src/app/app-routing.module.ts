6eb87ac2afd77346f6bc9ee914d11c728ae0e087
paosanchezmonroy <paola_sanchez_monroy@hotmail.com>
2023-05-31 14:45:47 -0600

    Reglas de ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent}];
+  {path: '', component: LandingPageComponent},
+  {path: 'generos',component: IndiceGenerosComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
