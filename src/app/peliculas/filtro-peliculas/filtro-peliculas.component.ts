b7fd2fe7e315c271cadad13acd03c22cfaa7f087
paosanchezmonroy <paola_sanchez_monroy@hotmail.com>
2023-06-02 16:48:07 -0600

    Iconos en menu

---
+++ src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-peliculas',
+  templateUrl: './filtro-peliculas.component.html',
+  styleUrls: ['./filtro-peliculas.component.css']
+})
+export class FiltroPeliculasComponent {
+
+}
